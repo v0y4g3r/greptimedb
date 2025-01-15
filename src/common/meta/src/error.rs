fc77b08b0db04bd5d87d353c62f42ff53b39564b
Lei, HUANG <mrsatangel@gmail.com>
2025-01-15 15:42:32 +0800

    feat/show-process-list:  Refactor Process Management in Meta Module
     - Introduced `ProcessManager` for handling process registration and deregistration.
     - Added methods for managing and querying process states, including `register_query`, `deregister_query`, and `list_all_processes`.
     - Removed redundant process management code from the query module.
     - Updated error handling to reflect changes in process management.
     - Enhanced test coverage for process management functionalities.

--- src/common/meta/src/error.rs
+++ src/common/meta/src/error.rs
@@ -709,7 +709,28 @@ pub enum Error {
         key: String,
         #[snafu(implicit)]
         location: Location,
-    }
+    },
+    // #[snafu(display("Failed to serialize process value when registering."))]
+    // RegisterProcess {
+    //     source: crate::error::Error,
+    //     #[snafu(implicit)]
+    //     location: Location,
+    // },
+    //
+    // #[snafu(display("Failed to list all running processes."))]
+    // ListProcesses {
+    //     source: crate::error::Error,
+    //     #[snafu(implicit)]
+    //     location: Location,
+    // },
+    //
+    // #[snafu(display("Failed to delete process {}", msg))]
+    // DeleteProcess {
+    //     msg: String,
+    //     source: crate::error::Error,
+    //     #[snafu(implicit)]
+    //     location: Location,
+    // },
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
@@ -815,7 +836,10 @@ impl ErrorExt for Error {
             | PostgresTransaction { .. }
             | PostgresTransactionRetryFailed { .. } => StatusCode::Internal,
             Error::DatanodeTableInfoNotFound { .. } => StatusCode::Internal,
-            InvalidProcessKey { .. } => StatusCode::Internal
+            InvalidProcessKey { .. } => StatusCode::Internal,
+            // RegisterProcess { .. } | ListProcesses { .. } | DeleteProcess { .. } => {
+            //     StatusCode::Internal
+            // }
         }
     }
 
